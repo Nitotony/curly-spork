6ee9a655c0fa906463692c3ea0e90d0e3adcb3a0
Nito Tony C <nitotony47@gmail.com>
2024-09-03 09:55:07 +0530

    alpha

--- src/test/java/my/first/web/services/restfulwebservices/alpha.java
+++ src/test/java/my/first/web/services/restfulwebservices/alpha.java
@@ -1,4 +1,12 @@
 package my.first.web.services.restfulwebservices;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class alpha {
+
+    public void display(){
+        System.out.println("alpha");
+    }
+
 }
