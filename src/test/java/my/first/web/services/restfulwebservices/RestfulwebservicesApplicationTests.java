c00fecc186e39d2e9348514fac0bfcfcacd2f508
Nito Tony C <nitotony47@gmail.com>
2024-09-03 09:34:56 +0530

    die

---
+++ src/test/java/my/first/web/services/restfulwebservices/RestfulwebservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package my.first.web.services.restfulwebservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestfulwebservicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
